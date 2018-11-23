f6463c6d44262c514232069179d8bce214157ec1
GuillermoGarcia <guillermogarciaf@gmail.com>
2018-11-23 12:48:33 +0100

    Save Character

--- app/src/main/java/com/example/ulric/avispro/actividades/ListActivity.java
+++ app/src/main/java/com/example/ulric/avispro/actividades/ListActivity.java
@@ -38,29 +38,27 @@ public class ListActivity extends AppCompatActivity {
     LinearLayoutManager manager = new LinearLayoutManager(this);
 
     recycler.setLayoutManager(manager);
+    final sheetsListAdapter adapter = new sheetsListAdapter(getApplicationContext(), R.layout.activity_list_sheet, personajes,
+      new sheetsListAdapter.OnItemClickListener() {
+        @Override
+        public void onItemClick(Personaje character, int position) {
+          Bundle bundle = new Bundle();
+          bundle.putSerializable("personaje", character);
+          bundle.putSerializable("usuario", usuario);
 
+          Intent intent = new Intent(ListActivity.this, SheetActivity.class);
+          intent.putExtras(bundle);
+          startActivity(intent);
+
+        }
+      }
+    );
+    recycler.setAdapter(adapter);
     usuario.cargarPersonajes(new MyCallbackPersonaje() {
       @Override
       public void onCallback(Personaje personaje) {
         personajes.add(personaje);
         Log.d("Callback: ", "Personaje cargado: " + personaje.getIdPersonaje());
-        sheetsListAdapter adapter = new sheetsListAdapter(getApplicationContext(), R.layout.activity_list_sheet, personajes,
-          new sheetsListAdapter.OnItemClickListener() {
-          @Override
-          public void onItemClick(Personaje character, int position) {
-
-            Bundle bundle = new Bundle();
-            bundle.putSerializable("personaje", character);
-            bundle.putSerializable("usuario", usuario);
-
-            Intent intent = new Intent(ListActivity.this, SheetActivity.class);
-            intent.putExtras(bundle);
-            startActivity(intent);
-
-            }
-          }
-        );
-        recycler.setAdapter(adapter);
         adapter.notifyDataSetChanged();
       }
     });
